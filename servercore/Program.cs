de4c8d67d49a483be7b616d71f33e422c836b2f9
ohne <ghdlr785@gmail.com>
2022-03-16 19:52:43 +0900

    초기 커밋.

---
+++ servercore/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    public class Program
+    {
+        
+        public static void Main(string[] args)
+        {
+            int[,] arr = new int[10000, 10000];
+
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                {
+                    for (int x = 0; x < 10000; x++)
+                    {
+                        arr[y, x] = 1;
+                    }
+                }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(y,x) 순서 걸린 시간 {end - now}");
+            }
+
+
+            {
+                long now = DateTime.Now.Ticks;
+                for (int y = 0; y < 10000; y++)
+                {
+                    for (int x = 0; x < 10000; x++)
+                    {
+                        arr[x, y] = 1;
+                    }
+                }
+                long end = DateTime.Now.Ticks;
+                Console.WriteLine($"(x,y) 순서 걸린 시간 {end - now}");
+            }
+        }
+    }
+}
