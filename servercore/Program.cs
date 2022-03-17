41d65d2acfb5e68b02f5468c2325ad5a7a3e9f84
ohne <ghdlr785@gmail.com>
2022-03-17 22:51:40 +0900

    interlocked

--- servercore/Program.cs
+++ servercore/Program.cs
@@ -4,52 +4,40 @@ using System.Linq;
 
 namespace MyApp // Note: actual namespace depends on the project name.
 {
-    //메모리 베리어
-    //1. 코드 재배치 억제
-    //2. 가시성
-
+    
     public class Program
     {
-        static int x = 0;
-        static int y = 0;
-        static int r1 = 0;
-        static int r2 = 0;
+        static int number = 0;
 
         static void Thread_1()
         {
-            y = 1;
-            Thread.MemoryBarrier();
-            r1 = x;
+            for (int i = 0; i < 100000; i++)
+            { 
+                Interlocked.Increment(ref number);
+                //number++;
+            }
         }
 
         static void Thread_2()
         {
-            x = 1;
-            Thread.MemoryBarrier();
-            r2 = y;
+            for (int i = 0; i < 100000; i++)
+            {
+                Interlocked.Decrement(ref number);
+                //number--;
+            }
         }
 
         public static void Main(string[] args)
         {
-            int count = 0;
-            while (true)
-            { 
-                count++;
-                x = y = r1 = r2 = 0;
-                Task t1 = new Task(Thread_1);
-                Task t2 = new Task(Thread_2);
+            Task t1 = new Task(Thread_1);
+            Task t2 = new Task(Thread_2);
 
-                t1.Start();
-                t2.Start();
+            t1.Start();
+            t2.Start();
 
-                Task.WaitAll(t1,t2);
+            Task.WaitAll(t1, t2);
 
-                if (r1 == 0 && r2 == 0)
-                {
-                    break;
-                }
-            }
-            Console.WriteLine($"{count}번만에 빠져나옴!");
+            Console.WriteLine(number);
         }
     }
 }
